094b4d170f8489239e5fb9181e6ac1c848179ee5
snlo <snloveydus@sina.com>
2018-05-15 12:06:11 +0800

    添加取消动态部署宏定义

--- SNTool/SNTool/SNTololMacro.h
+++ SNTool/SNTool/SNTololMacro.h
@@ -46,7 +46,10 @@ _Pragma("clang diagnostic push") \
 _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
 if ([UIScrollView instancesRespondToSelector:NSSelectorFromString(@"setContentInsetAdjustmentBehavior:")]) {\
 [scrollView   performSelector:NSSelectorFromString(@"setContentInsetAdjustmentBehavior:") withObject:@(2)];\
+_Pragma("clang diagnostic pop") \
 } else {\
+_Pragma("clang diagnostic push") \
+_Pragma("clang diagnostic ignored \"-Wdeprecated\"") \
 vc.automaticallyAdjustsScrollViewInsets = NO;\
 }\
 _Pragma("clang diagnostic pop") \
